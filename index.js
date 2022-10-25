dcf509990f1926ca4325b007cbf098b29b269f12
levi2up <115722206+levi2up@users.noreply.github.com>
2022-10-25 11:13:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 fetch(‘https://sky.shiiyu.moe/api/v2/bazaar’)
    .then(response => response.json())
    .then(items => showItems(items.results));
+
+showItems = items => {
+  const itemsDiv = document.querySelector(‘#Bazaar’);
+  characters.forEach(items => {
+    const itemElement = document.createElement(‘p’);
+    itemElement.innerText = `Item Name: ${item.Name}`
+    itemsDiv.append(itemElement);
+  });
+}
